2d26080360e524d0804374f713571799fc6026f3
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-09-06 14:16:34 +0700

    Add validations for Product and Category models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :inventories, dependent: :destroy
   has_many :products, through: :inventories
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   has_many :inventories, dependent: :destroy
   has_many :categories, through: :inventories
+
+  validates :name, presence: true
+  validates :price, presence: true
 end
