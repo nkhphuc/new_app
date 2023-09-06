9f504c12f660a61a937382d10041986d85723e38
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-09-06 13:49:30 +0700

    Create Product, Category, Inventory models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :inventories
+  resources :categories
+  resources :products
   resources :blogs
   root 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
