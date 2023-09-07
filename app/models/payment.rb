817458d0ed7c5fce1862ba6c349a96cdb6afdd92
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-09-07 17:00:27 +0700

    Define Payment total method and add accepted nested attributes for PaymentDetail.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,4 +4,15 @@
 class Payment < ApplicationRecord
   belongs_to :customer
   has_many :payment_details, dependent: :destroy
+  accepts_nested_attributes_for :payment_details
+
+
+  before_save :set_total
+
+  def set_total
+    self.total = payment_details.map(&:menu).sum(&:price)
+  end
+
+  # Example Payment:
+  # payment = { name: 'payment_name2', customer_id: 1, payment_details_attributes: [{ menu_type: 'Timeprice', menu_id: 1 }, { menu_type: 'Coupon', menu_id: 1 }] }
 end
